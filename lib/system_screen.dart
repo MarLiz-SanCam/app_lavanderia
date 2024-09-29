2844bd726d79cac2e428b2d7df6dfcd2435b018d
Marliz <marina.liz.santinicamarena@gmail.com>
2024-09-28 19:42:50 -0600

    added grid view to the app

--- lib/system_screen.dart
+++ lib/system_screen.dart
@@ -51,9 +51,72 @@ class Sistema extends StatelessWidget {
         ),
       ),
       body: Center(
-        
-        child: Text('Bienvenido al Sistema de Lavandería'),
+        child: GridView.builder(
+          padding: const EdgeInsets.all(20.0),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 20.0,
+            mainAxisSpacing: 20.0,
+          ),
+          itemCount: 2,
+         itemBuilder: (context, index) {
+  return Card(
+    color: Colors.white,
+    elevation: 10,
+    margin: EdgeInsets.all(8),
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //TODO: volver los íconos dinámicos
+              Icon(Icons.star, size: 24), // Ícono
+              SizedBox(width: 8), // Espacio entre el ícono y el título
+              Text(
+                'Pedido $index',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+          SizedBox(height: 16), // Espacio entre el título y los botones
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para el primer botón
+                },
+                child: Text('Botón 1'),
+              ),
+              SizedBox(height: 8), // Espacio entre los botones
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para el segundo botón
+                },
+                child: Text('Botón 2'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+        )
       ),
     );
   }
 }
+//TODO: Agregar un botón flotante después de completar el grid
+
+/* floatingActionButton: FloatingActionButton(
+  onPressed: () {
+    // Acción al presionar el botón
+  },
+  backgroundColor: colors.accentuated,
+  child: Icon(Icons.add),
+), */
